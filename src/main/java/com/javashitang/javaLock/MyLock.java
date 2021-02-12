9dd8dad12b7788f73e21f51a3f2d0d3814c97170
lilimin <290059742@qq.com>
2021-02-12 23:06:25 +0800

    增加注释

--- src/main/java/com/javashitang/javaLock/MyLock.java
+++ src/main/java/com/javashitang/javaLock/MyLock.java
@@ -13,6 +13,7 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class MyLock {
 
+    // 0表示锁没有被获取，1表示锁被获取
     private volatile int state;
     // 这里应该用并发安全的容器，这里只是举例
     private List<Thread> threadList = new ArrayList<>();
