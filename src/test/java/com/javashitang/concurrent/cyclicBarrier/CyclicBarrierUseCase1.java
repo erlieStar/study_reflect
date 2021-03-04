d0e33b6c9aca453a0ffc4c5ed8a748060cf39d74
lilimin <290059742@qq.com>
2021-03-04 16:42:51 +0800

    完善例子

--- src/test/java/com/javashitang/concurrent/cyclicBarrier/CyclicBarrierUseCase1.java
+++ src/test/java/com/javashitang/concurrent/cyclicBarrier/CyclicBarrierUseCase1.java
@@ -9,19 +9,19 @@ import java.util.concurrent.*;
  */
 public class CyclicBarrierUseCase1 {
 
-    public static void main(String[] args) throws InterruptedException{
-        CyclicBarrier barrier = new CyclicBarrier(5);
-        ExecutorService service = Executors.newFixedThreadPool(5);
+    public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(4);
+        ExecutorService service = Executors.newCachedThreadPool();
         Random random = new Random();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 8; i++) {
             int num = i;
             service.submit(() -> {
                 try {
+                    System.out.println(num + " 准备去麻将馆");
                     TimeUnit.SECONDS.sleep(random.nextInt(5));
-                    System.out.println(num + " 号运动员完成比赛");
+                    System.out.println(num + " 到达麻将馆");
                     barrier.await();
-                    barrier.reset();
-                    System.out.println("运动员都跑完，裁判宣布比赛结束");
+                    System.out.println(num + " 开始打");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
