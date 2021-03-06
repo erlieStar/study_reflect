05d4e6a1473756cdc66b5ecdad85abb42709e722
lilimin <290059742@qq.com>
2021-03-06 22:06:22 +0800

    增加管程例子

---
+++ src/test/java/com/javashitang/concurrent/readWriteLock/ReadWriteLockUseCase.java
@@ -0,0 +1,12 @@
+package com.javashitang.concurrent.readWriteLock;
+
+/**
+ * @author lilimin
+ * @since 2021-03-06
+ */
+public class ReadWriteLockUseCase {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/javashitang/monitor/Semaphore.java
@@ -0,0 +1,27 @@
+package com.javashitang.monitor;
+
+/**
+ * @author lilimin
+ * @since 2021-03-06
+ */
+//public class Semaphore {
+//
+//    private int sem;
+//    private WaitQueue q;
+//
+//    public void p() {
+//        sem--;
+//        if (sem < 0) {
+//            add this thread t to q;
+//            block(t);
+//        }
+//    }
+//
+//    public void v() {
+//        sem++;
+//        if (sem <= 0) {
+//            remove a thread t from q;
+//            wakeup(t)
+//        }
+//    }
+//}
